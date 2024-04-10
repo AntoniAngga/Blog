92e7d253716fd7065120eccfa0d71fe4e6c4028b
AntoniAngga <antoniangga14@gmail.com>
2024-04-10 13:53:21 +0700

    CRUD Blog done

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,57 @@
+class BlogPostsController < ApplicationController
+  before_action :set_before_action_blog_post, only: %i[show edit update destroy]
+
+  def index
+    @blog_posts = BlogPost.all
+  end
+
+  def new
+    @blog_post = BlogPost.new
+  end
+
+  def show; end
+
+  def create
+    @blog_post = BlogPost.new(blog_post_params)
+    if @blog_post.valid?
+      @blog_post.save
+      flash[:notice] = 'Blog Post Sucess Input'
+      redirect_to blog_post_path(@blog_post.id)
+    else
+      flash[:notice] = 'Blog Post input error'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  rescue ActiveRecord::RecordNotFound
+    flash[:notice] = 'Record not found'
+    redirect_to root_path
+  end
+
+  def update
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog_post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
+  end
+
+  def set_before_action_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:notice] = 'Record not found'
+    redirect_to root_path
+  end
+end
--- db/migrate/20240402070410_create_blog_posts.rb
+++ db/migrate/20240402070410_create_blog_posts.rb
@@ -2,7 +2,7 @@ class CreateBlogPosts < ActiveRecord::Migration[7.1]
   def change
     create_table :blog_posts do |t|
       t.string :title
-      t.string :body
+      t.text :body
 
       t.timestamps
     end
